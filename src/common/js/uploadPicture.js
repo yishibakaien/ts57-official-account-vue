8b738f494de4ecbf09e692887c714af65c5643e0
cloud-cb <646932161@qq.com>
2017-06-08 14:55:40 +0800

    新增api配置

--- src/common/js/uploadPicture.js
+++ src/common/js/uploadPicture.js
@@ -1,23 +1,38 @@
-// create by cloud_cb on 2017/04/20.
 'use strict';
+/**
+ * 返回图片base64地址
+ */
+// function uploadPictrue(file, resolve) {
+//     if (file.files && file.files[0]) {
+//         var reader = new FileReader();
+//         reader.onload = function(evt) {
+//             // alert('转码完毕图片地址为:', evt.target.result);
+//             resolve(evt.target.result);
+//         };
+//         reader.readAsDataURL(file.files[0]);
+//     }
+// }
+// module.exports = uploadPictrue;
+//
 
 // 上传图片，传入input[type=file] 的onchange 事件
 // 返回一个 Promise 对象
 function uploadPictrue(e) {
-	return _getBase64Image(e);
+  return _getBase64Image(e);
 }
 
 function _getBase64Image(file) {
-	let reader = new FileReader();
-	return new Promise((resolve, reject) => {
-		reader.onload = (evt) => {
-			resolve(evt.target.result);
-		};
-		reader.onerror = (evt) => {
-			reject(evt);
-		};
-		reader.readAsDataURL(file);
-	});
+  let reader = new FileReader();
+  return new Promise((resolve, reject) => {
+    reader.onload = (evt) => {
+      resolve(evt.target.result);
+    };
+    reader.onerror = (evt) => {
+      reject(evt);
+    };
+    reader.readAsDataURL(file);
+  });
 }
 
 export default uploadPictrue;
+
--- src/pages/picSearch/picSearch.vue
+++ src/pages/picSearch/picSearch.vue
@@ -1,42 +1,121 @@
 <template>
   <div class="pic-search-page">
     <div class="search-category">
-
-    </div>
-    <div class="text">最近搜花记录</div>
-    <div class="search-history">
-      <div class="history-img" :style="{backgroundImage:'url(' + url + ')'}">
-
+      <input type="file" capture="camera" name="file" ref="file" @change="uploadPic" style="display:none;">
+      <div class="pic-wrapper border1px" :style="{backgroundImage:'url(' + cropPic + ')'}" @click="choosePic">
+        <i class="iconfont icon-xiangji" :style="{display: cropPic ? 'none' : 'inline-block'}"></i>
       </div>
+    </div>
+    <div class="search-button-box">
+      <button class="button button-blue" v-for="item in category" @click="doSearch(item.category)">{{item.text}}</button>
     </div>
   </div>
 </template>
 
 <script>
+// AlloyTeam 图片裁剪
+import AlloyCrop from '../../common/js/crop/crop';
+import {
+  encoded
+} from '../../common/api/api';
 export default {
   data() {
     return {
-      url: require('../../../static/images/patterns1.png'),
-      category: ['搜面料', '搜大边', '搜小边', '搜睫毛']
+      category: [
+        {text: '搜面料', category: 100010},
+        {text: '搜大边', category: 100011},
+        {text: '搜小边', category: 100012},
+        {text: '搜睫毛', category: 100013}
+      ],
+      cropPic: ''
     };
+  },
+  methods: {
+    choosePic() {
+      this.$refs.file.click();
+    },
+    uploadPic() {
+      var file = this.$refs.file.files[0];
+      if (file) {
+        var reader = new FileReader();
+        reader.onload = () => {
+          var url = reader.result;
+          this.handleChoosePic(url);
+        };
+        reader.readAsDataURL(file);
+      } else {
+        // this.$toast('出错');
+      }
+    },
+    handleChoosePic(url) {
+      var _this = this;
+      /* eslint-disable no-new */
+      new AlloyCrop({
+          image_src: url,
+          circle: false,
+          width: 240,
+          height: 240,
+          ok: function(base64, canvas) {
+              _this.cropPic = base64;
+          },
+          cancel: function() {},
+          ok_text: '截取',
+          cancel_text: '取消'
+      });
+    },
+    doSearch(category) {
+      if (!this.cropPic) {
+        alert('请先上传一张图片');
+        return;
+      }
+
+      encoded({
+        category: category,
+        searchType: 300,
+        encoded: this.cropPic
+      }, function(res) {
+        console.log('搜图返回结果', res);
+      }, function(res) {
+        console.log('返回结果失败', res);
+      });
+    }
   }
 };
 </script>
 <style lang="stylus">
 @import '../../common/styles/mixin.styl';
+
 .search-category
-  display flex
-  padding 20px 0
-  text-align center
   background #fff
-  .category
-    flex 1
-    .item
-      display inline-block
-      font-size 14px
-      padding 8px 12px
-      border-radius 4px
-      border 1px solid #666
+  position relative
+  height 100px
+  padding-top 20px
+  .pic-wrapper
+    center()
+    centerPic()
+    border1px()
+    display inline-block
+    height 30px
+    width 30px
+    padding 32px
+    // border 1px solid #d8d8d8
+    background-color #f2f2f2
+    border-radius 2px
+    .icon-xiangji
+      position relative
+      font-size 30px
+      color #aaa
+.search-button-box
+  background #fff
+  padding 12px 0
+  .button
+    display inline-block
+    padding 4px 8px
+    font-size 14px
+    width 20%
+    margin-left 5%
+    &:first-child
+      margin-left 2.5%
 .text
   padding 14px
   font-size 14px
